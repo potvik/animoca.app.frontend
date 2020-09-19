88465f8822aca5284c8d963b3e0619ce7b2452bc
jenya <hypnagonia@gmail.com>
2020-09-19 13:44:03 +0300

    retweet

--- src/pages/PlayersMarketplace/BuyLootBoxModal/index.tsx
+++ src/pages/PlayersMarketplace/BuyLootBoxModal/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Box } from 'grommet';
-import { Title, Text, Icon } from 'components/Base';
-import { observer } from 'mobx-react-lite';
-import { useStores } from 'stores';
-import { PlayerCardLite } from './PlayerCardLite';
+import {Box} from 'grommet';
+import {Title, Text, Icon} from 'components/Base';
+import {observer} from 'mobx-react-lite';
+import {useStores} from 'stores';
+import {PlayerCardLite} from './PlayerCardLite';
 import * as styles from './card.styl';
-import { EXPLORER_URL } from '../../../blockchain';
-import { Spinner } from 'ui';
-import { truncateAddressString } from '../../../utils';
-import { PlayerCardLiteOwner } from './PlayerCardLiteOwner';
-import { useMediaQuery } from 'react-responsive';
-interface IBuyPlayerModalProps {}
+import {EXPLORER_URL} from '../../../blockchain';
+import {Spinner} from 'ui';
+import {truncateAddressString} from '../../../utils';
+import {PlayerCardLiteOwner} from './PlayerCardLiteOwner';
+import {useMediaQuery} from 'react-responsive';
+import {  TwitterShareButton } from 'react-twitter-embed'
+
+interface IBuyPlayerModalProps {
+}
 
 const Description = observer(() => {
-  const { tokenList: buyPlayer } = useStores();
+  const {tokenList: buyPlayer} = useStores();
 
-  const isSmallMobile = useMediaQuery({ query: '(max-width: 678px)' });
+  const isSmallMobile = useMediaQuery({query: '(max-width: 678px)'});
 
-  let icon = () => <Icon style={{ width: 50 }} glyph="RightArrow" />;
+  let icon = () => <Icon style={{width: 50}} glyph="RightArrow" />;
   let description = 'Approval';
 
   switch (buyPlayer.actionStatus) {
@@ -25,7 +28,7 @@ const Description = observer(() => {
       icon = () =>
         isSmallMobile ? null : (
           <Icon
-            style={{ height: '110px', width: 'auto' }}
+            style={{height: '110px', width: 'auto'}}
             className={styles.sendArrow}
             glyph="SendArrow"
           />
@@ -40,12 +43,12 @@ const Description = observer(() => {
       break;
 
     case 'error':
-      icon = () => <Icon size="50" style={{ width: 50 }} glyph="Alert" />;
+      icon = () => <Icon size="50" style={{width: 50}} glyph="Alert" />;
       description = buyPlayer.error;
       break;
 
     case 'success':
-      icon = () => <Icon size="50" style={{ width: 50 }} glyph="CheckMark" />;
+      icon = () => <Icon size="50" style={{width: 50}} glyph="CheckMark" />;
       description = 'Success';
       break;
   }
@@ -55,29 +58,38 @@ const Description = observer(() => {
       direction="column"
       align="center"
       justify="center"
-      style={{ maxWidth: 340, margin: '20px 0' }}
+      style={{maxWidth: 340, margin: '20px 0'}}
     >
       {icon()}
       <Box className={styles.description}>
         <Text>{description}</Text>
         {buyPlayer.txId ? (
           <a
-            style={{ marginTop: 10 }}
+            style={{marginTop: 10}}
             href={EXPLORER_URL + `/tx/${buyPlayer.txId}`}
             target="_blank"
           >
             Tx id: {truncateAddressString(buyPlayer.txId)}
           </a>
         ) : null}
+
+        {/*{description === 'Success' && <>
+          <TwitterShareButton
+            url={'https://facebook.com/saurabhnemade'}
+            size='large'
+            options={{ text: '#reactjs is awesome', via: 'saurabhnemade', size: 'large' }}
+          />
+        </>}*/}
+
       </Box>
     </Box>
   );
 });
 
 export const BuyLootBoxModal = observer<IBuyPlayerModalProps>(props => {
-  const { tokenList: buyPlayer } = useStores();
-  const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
-  const isSmallMobile = useMediaQuery({ query: '(max-width: 678px)' });
+  const {tokenList: buyPlayer} = useStores();
+  const isMobile = useMediaQuery({query: '(max-width: 1000px)'});
+  const isSmallMobile = useMediaQuery({query: '(max-width: 678px)'});
 
   if (isSmallMobile) {
     return (
@@ -88,7 +100,7 @@ export const BuyLootBoxModal = observer<IBuyPlayerModalProps>(props => {
         gap="20px"
         pad="medium"
       >
-        <Title style={{ textAlign: 'center' }}>Buy Loot Box</Title>
+        <Title style={{textAlign: 'center'}}>Buy Loot Box</Title>
         <PlayerCardLite />
         <Description />
       </Box>
@@ -103,20 +115,20 @@ export const BuyLootBoxModal = observer<IBuyPlayerModalProps>(props => {
       }}
       className={styles.modalContainer}
     >
-      <Title style={{ textAlign: 'center' }}>Buy Loot Box</Title>
+      <Title style={{textAlign: 'center'}}>Buy Loot Box</Title>
       <Box
-        margin={{ vertical: isMobile ? '' : 'large' }}
+        margin={{vertical: isMobile ? '' : 'large'}}
         direction="row"
         align="center"
         justify="between"
       >
-        <Box style={{ transform: isMobile ? 'scale(0.8)' : '' }}>
+        <Box style={{transform: isMobile ? 'scale(0.8)' : ''}}>
           <PlayerCardLite />
         </Box>
-        <Box style={{ transform: isMobile ? 'scale(0.8)' : '' }}>
+        <Box style={{transform: isMobile ? 'scale(0.8)' : ''}}>
           <Description />
         </Box>
-        <Box style={{ transform: isMobile ? 'scale(0.8)' : '' }}>
+        <Box style={{transform: isMobile ? 'scale(0.8)' : ''}}>
           <PlayerCardLiteOwner />
         </Box>
       </Box>
